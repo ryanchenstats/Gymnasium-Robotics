ebc1b1fbcb7f8662820eae5f5532cacba0176b90
ryanchenstats <ryanchenitis@gmail.com>
2023-07-27 21:54:16 -0500

    added custom map for point env

--- gymnasium_robotics/envs/maze/point_maze.py
+++ gymnasium_robotics/envs/maze/point_maze.py
@@ -316,7 +316,6 @@ class PointMazeEnv(MazeEnv, EzPickle):
             point_xml_file_path = path.join(
                 path.dirname(path.realpath(__file__)), "../assets/point/point_SAM.xml"
             )
-            print('SAM')
         else:
             point_xml_file_path = point_xml_file_path = path.join(
                 path.dirname(path.realpath(__file__)), "../assets/point/point.xml"
